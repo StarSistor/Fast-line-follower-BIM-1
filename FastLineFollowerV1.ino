5cefbc081a24abc7e4e63ade7990f31869696152
StarSistor <114177409+StarSistor@users.noreply.github.com>
2023-06-07 19:42:50 -0500

    Update FastLineFollowerV1.ino

--- FastLineFollowerV1.ino
+++ FastLineFollowerV1.ino
@@ -16,11 +16,9 @@
 #define EMITTER_PIN 10
 
 // Constantes del control PID
-#define KP 0.0035
-
-// Constante proporcional
-#define KD 0.0058 // Constante derivativa
-#define KI 0.00001  // Constante integral
+#define KP 4.6// Constante proporcional
+#define KD 19.4 // Constante derivativa 
+#define KI 0.009  // Constante integral
 
 QTRSensors qtr;
 
@@ -29,7 +27,7 @@ int lastError = 0;
 int integral = 0;
 
 void setup() {
-  // Set output pins for TB6612FNG motor controller
+// Establecer pines de salida para el driver TB6612FNG
   pinMode(motorAInput1, OUTPUT);
   pinMode(motorAInput2, OUTPUT);
   pinMode(motorBInput1, OUTPUT);
@@ -39,30 +37,28 @@ void setup() {
   pinMode(STBY, OUTPUT);
 
   digitalWrite(STBY, HIGH);
-  // Initialize QTR-8A sensor
+  // Inicializa el sensor QTR-8A 
   qtr.setTypeAnalog();
   qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5, A6, A7}, NUM_SENSORS);
   qtr.setEmitterPin(EMITTER_PIN);
   
-  digitalWrite(LED_BUILTIN, HIGH); // turn off Arduino's LED to indicate we are through with calibration
-
-  // Start serial communication
-  Serial.begin(9600);
+  digitalWrite(LED_BUILTIN, HIGH); // enciende LED de Arduino para indicar el inicio de la calibracion
+ 
+  Serial.begin(9600);// inicio de comunicacion serial
   
     for (int i = 0; i < 250; i++) {
     qtr.calibrate();
     delay(10);
   }
-    digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
-
+    digitalWrite(LED_BUILTIN, LOW); apaga el led Arduino para indicar termino de calibracion
 }
 
 
 void loop() {
-  // Read sensor values
+  // Leer valores del sensor
   uint16_t position = qtr.readLineBlack(sensorValues);
 
-  // Calculate error (weighted center position)
+  // Calcular el error (posición central ponderada)
   int weightedSum = 0;
   int sum = 0;
   for (int i = 0; i < NUM_SENSORS; i++) {
@@ -71,19 +67,19 @@ void loop() {
   }
   int error = position - (NUM_SENSORS - 1) * 1000 / 2;
 
-  // Calculate PID control components
+  // Calcular componentes de control PID
   int proportional = KP * error;
   int derivative = KD * (error - lastError);
   integral += error;
   int integralComponent = KI * integral;
 
-  // Calculate control signal
+  // Calcular señal de control
   int controlSignal = proportional + derivative + integralComponent;
 
-  // Control motors using the control signal
-  int motorSpeed = 225; // Adjust this value to control the forward speed
+  // Controlar motores usando la señal de control
+  int motorSpeed = 255; //Ajuste este valor para controlar la velocidad de avance//130
 
- // Control motors using the control signal
+ // Control de motores usando la señal de control
 int motorSpeedA = motorSpeed - controlSignal;
 int motorSpeedB = motorSpeed + controlSignal;
 
@@ -91,46 +87,24 @@ int motorSpeedB = motorSpeed + controlSignal;
 motorSpeedA = constrain(motorSpeedA, 0, 255);
 motorSpeedB = constrain(motorSpeedB, 0, 255);
 
-// Control the motors
-if (controlSignal > 4) {
-  // Turn right
+// Control de motores
+if (controlSignal >2) {
+  //gira derecha
   digitalWrite(motorBInput1, LOW);
   digitalWrite(motorBInput2, HIGH);
-  digitalWrite(motorAInput1, HIGH);//LOW para avanzar hacia adelante
-  digitalWrite(motorAInput2, LOW);
+  digitalWrite(motorAInput1, LOW);//LOW para avanzar hacia adelante
+  digitalWrite(motorAInput2, HIGH);
   analogWrite(motorAPWM, motorSpeedA);
   analogWrite(motorBPWM, motorSpeedB);
 } 
-if (controlSignal < -4 ) {
-  // Turn left
-  digitalWrite(motorBInput1, HIGH);
-  digitalWrite(motorBInput2, LOW);
+if (controlSignal < -2 ) {
+  // gira izquierda
+  digitalWrite(motorBInput1, LOW);
+  digitalWrite(motorBInput2, HIGH);
   digitalWrite(motorAInput1, LOW);
   digitalWrite(motorAInput2, HIGH); //LOW para avanzar hacia adelante
   analogWrite(motorAPWM, motorSpeedA);
   analogWrite(motorBPWM, motorSpeedB);
 }
 
-if (controlSignal >= -4 && controlSignal <= 4) {
-  // Turn left
-  digitalWrite(motorBInput1, LOW);
-  digitalWrite(motorBInput2, HIGH);
-  digitalWrite(motorAInput1, LOW);
-  digitalWrite(motorAInput2, HIGH);
-  analogWrite(motorAPWM, motorSpeedA);
-  analogWrite(motorBPWM, motorSpeedB);
-}
-
-  // Update last error
-  lastError = error;
-
-  // Print sensor values and position to the serial monitor
-  for (int i = 0; i < NUM_SENSORS; i++) {
-    Serial.print(sensorValues[i]);
-    Serial.print('\t');
-  }
-  Serial.println(controlSignal);
-  
-
-  //delay(2);
 }
